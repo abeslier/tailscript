ef00f98a695d09f84bf5e52f4cc06134f6adb913
unknown <Adrien.Beslier@ulb.be>
2024-09-17 01:58:35 +0200

    var correction in base

--- base.cmd
+++ base.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-const "tailscale=C:\Program Files\Tailscale\tailscale.exe"
+set "tailscale=C:\Program Files\Tailscale\tailscale.exe"
 if not exist %tailscale-path% (
     curl -L -o tailscale-setup.exe https://pkgs.tailscale.com/stable/tailscale-setup-latest.exe
     tailscale-setup.exe /quiet
