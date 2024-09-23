489da88357cf8d1d3dbadbafb142497451b6b0bd
unknown <Adrien.Beslier@ulb.be>
2024-09-23 16:46:58 +0200

    Windows base script correction

--- base.cmd
+++ base.cmd
@@ -1,13 +1,9 @@
 @echo off
-
 set tailscale="C:\Program Files\Tailscale\tailscale.exe"
-if not exist %tailscale-path% (
+if not exist %tailscale% (
     curl -L -o tailscale-setup.exe https://pkgs.tailscale.com/stable/tailscale-setup-latest.exe
     tailscale-setup.exe /quiet
     del tailscale-setup.exe
 ) else (
     %tailscale% logout
 )
-::tailscale.exe up --authkey ... --hostname ...
-
-pause
