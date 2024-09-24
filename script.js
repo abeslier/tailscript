55576424fb512fb47d1bc831674c8966795a180f
unknown <Adrien.Beslier@ulb.be>
2024-09-24 02:36:50 +0200

    os select

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ if not exist %tailscale% (
 ) else (
     %tailscale% logout
 )`
+const scriptWindowsCommand = "%tailscale% up"
+const scriptWindowsFilename = "tailscript.cmd"
 
 const command = "tailscale up"
-const commandWindows = "%tailscale% up"
 const flags = {
     authkey: "--authkey",
     hostname: "--hostname",
@@ -21,22 +22,23 @@ const inputs = {
     hostname: document.getElementById("hostnameInput"),
     subnets: document.getElementById("subnetInput")
 }
-
+const selectOS = document.getElementById("selectOS")
 
 function makeCMD(cmd) {
     for (let key in inputs) {
         if (inputs[key].value) {
             cmd += ` ${flags[key]} ${inputs[key].value.trim()}`
+            //cmd += ` ${flags[key]}=${inputs[key].value.trim()}`
         }
     }
     return cmd
 }
 
-function downloadAsFile(content) {
-    const blob = new Blob(content, { type: 'text/plain' });
+function downloadAsFile(content, filename) {
+    const blob = new Blob([content], { type: 'text/plain' });
     const a = document.createElement('a');
     a.href = URL.createObjectURL(blob);
-    a.download = 'tailscript.cmd';
+    a.download = filename;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
@@ -49,5 +51,26 @@ document.getElementById("copyButton").onclick = () => {
 }
 
 document.getElementById("downloadButton").onclick = () => {
-    downloadAsFile([scriptWindows, "\n", makeCMD(commandWindows)])
+    switch (selectOS.value) {
+        case "windows":
+            downloadAsFile(`${scriptWindows}\n${makeCMD(scriptWindowsCommand)}`, scriptWindowsFilename);
+            break;
+        case "debian-bookworm":
+            break;
+    }
+}
+
+/* temporary, until debian script implemented */
+function downloadButtonState() {
+    if (selectOS.value === "debian-bookworm") {
+        document.getElementById("downloadButton").setAttribute("disabled", true);
+    } else {
+        document.getElementById("downloadButton").removeAttribute("disabled");
+    }
+}
+selectOS.onchange = () => {
+    downloadButtonState();
+}
+window.onload = () => {
+    downloadButtonState();
 }
