6272fa11472b40f84d0703076869f0d09e9e7b99
unknown <Adrien.Beslier@ulb.be>
2024-09-26 17:34:41 +0200

    removed base file, smoother theme button animation, demo fix

--- base.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-set tailscale="C:\Program Files\Tailscale\tailscale.exe"
-if not exist %tailscale% (
-    curl -L -o tailscale-setup.exe https://pkgs.tailscale.com/stable/tailscale-setup-latest.exe
-    tailscale-setup.exe /quiet
-    del tailscale-setup.exe
-) else (
-    %tailscale% logout
-)
